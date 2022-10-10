8dbd1c3850357b69569348411622e3b00d90420d
edwinperez16 <114115684+Gaelpb17@users.noreply.github.com>
2022-10-09 19:29:57 -0500

    terminado 1-3
    videos del 1 al 3 acabados

--- empleos/src/main/java/net/itinajero/controller/HomeController.java
+++ empleos/src/main/java/net/itinajero/controller/HomeController.java
@@ -1,19 +1,16 @@
 package net.itinajero.controller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import net.itinajero.model.Vacante;
 import net.itinajero.service.IVacantesService;
-
-import org.springframework.ui.Model;
 
 @Controller
 public class HomeController {
---
+++ empleos/src/main/java/net/itinajero/service/db/CategoriaServiceJpa.java
@@ -0,0 +1,42 @@
+package net.itinajero.service.db;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.itinajero.model.Categoria;
+import net.itinajero.repository.CategoriasRepository;
+import net.itinajero.service.ICategoriasService;
+
+@Service
+public class CategoriaServiceJpa implements ICategoriasService {
+	
+	@Autowired
+	private CategoriasRepository CategoriasRepo;
+
+	@Override
+	public void guardar(Categoria categoria) {
+		// TODO Auto-generated method stub
+		CategoriasRepo.save(categoria);
+
+	}
+
+	@Override
+	public List<Categoria> buscarTodas() {
+		// TODO Auto-generated method stub
+		return CategoriasRepo.findAll();
+	}
+
+	@Override
+	public Categoria buscarPorId(Integer idCategoria) {
+		// TODO Auto-generated method stub
+		Optional<Categoria> optional = CategoriasRepo.findById(idCategoria);
+		if(optional.isPresent()){
+			return optional.get();
+		}
+		return null;
+	}
+
+}
